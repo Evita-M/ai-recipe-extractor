67ffe5fcf6222c10cbb4d157cb477d16b2288b20
Eva Matova <eva.matova6@gmail.com>
2025-06-21 13:43:53 +0300

    Remove not used function

--- apps/web/lib/utils/is-supported-language.ts
+++
@@ -1,7 +0,0 @@
-import { supportedLanguages } from '../types/language';
-
-export function isSupportedLanguage(
-  lang: string
-): lang is (typeof supportedLanguages.options)[number] {
-  return supportedLanguages.options.includes(lang as any);
-}
